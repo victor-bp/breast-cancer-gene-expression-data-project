8556973f8d862cd32788b5f166968d47cb5a14f4
victor-bp <victorbpoulsen@gmail.com>
2026-04-10 15:42:52 +0200

    cleaned up code and added another experiment with adaptive validation and MSE of the best stationary guess

--- breast_cancer.R
+++ breast_cancer.R
@@ -26,10 +26,14 @@ set.seed(1)
 train <- sample(1:nrow(x), round(nrow(x) * 0.8))
 test <- (-train)
 
+
+
 # to start off we do least squares regression as a baseline 
 library(glmnet)
 set.seed(1) # seed because there is more than one solution when p > n
 lsquares.fit <- glmnet(x[train, ], y$target[train], alpha=0, lambda=0)
+
+
 
 # next we will do ridge regression 
 #  to select our tuning parameter lambda for the prior l2 norm term we will use cv.glmnet to calculate 
@@ -46,16 +50,66 @@ ridge.min_lambda
 ridge.fit <- glmnet(x[train, ], y$target[train], alpha=0, lambda=ridge.min_lambda)
 
 
+
 # next we will do lasso regression
-#  to select our tuning parameter lambda for the prior l1 norm term we will first try using adaptive validation 
-#  this should give us better support recovery, but for prediction cross validation should perform better
+# to select our tuning parameter we will do cross validation
+set.seed(1)
+wide_grid <- 10^seq(-5, 1, length=200)
+cv.out <- cv.glmnet(x[train, ], y$target[train], nfolds=5, alpha=1, lambda=wide_grid)
+plot(cv.out)
 
-l <- function (x) max(abs(x[-1, ])) # the maximum norm is used for lasso adaptive validation
-                                    #  this is because we are trying to do support recovery and 
-                                    #  because we use the maximum norm we can capture all "reasonably large predictors"
-                                    #  see fundamentals of high dimensional statistics example 4.4.1 & chapter 7.4
+lasso.cv.min_lambda <- cv.out$lambda.min
+lasso.cv.min_lambda
 
-max_lambda <- l(t(x) %*% y$target)/dim(x)[1] # we choose the maximum lambda to be the smallest lambda 
+# we can then fit on all our data
+lasso.cv.fit <- glmnet(x[train, ], y$target[train], alpha=1, lambda=lasso.cv.min_lambda )
+
+
+
+
+
+# we can also use adaptive validation to select our tuning parameter for the lasso, 
+#  although this will probably be worse for prediction it should be better for support recovery
+# we perform adaptive validation as in algorithm 4.4 from fumdamentals from high dimensional statistics
+adaptive_validation <- function(loss, lambda_grid, beta_hats, factor) {
+  r <- max(lambda_grid)
+  
+  r_av <- 0
+  r_av_found <- FALSE
+  r_test <- 0
+  
+  while ((r != min(lambda_grid)) && (!r_av_found)) {
+    beta_hat_r <- coef(beta_hats, s=r)
+    r_prime <- max(lambda_grid)
+    message(sprintf("r: %s\n", r))
+    while ((r_prime > r) && (!r_av_found)) {
+      beta_hat_r_prime <- coef(beta_hats, s=r_prime)
+      
+      # debug
+      #message(sprintf("r: %s\n", r))
+      #message(sprintf("r_prime: %s\n", r_prime))
+      #message(sprintf("l(coef(beta_hat_r_prime) - coef(beta_hat_r)): %s\n", l(beta_hat_r_prime - beta_hat_r)))
+      #message(sprintf("factor * r_prime + factor * r: %s\n", factor * r_prime + factor * r))
+      
+      if (loss(beta_hat_r_prime - beta_hat_r) > factor * r_prime + factor * r) {
+        r_av_found <- TRUE #exit from both loops 
+        
+        # before we exit we choose the last r for which the condition was still fulfilled
+        r <- min(lambda_grid[lambda_grid > r]) 
+        r_av <- r 
+      }
+      r_prime <- max(lambda_grid[lambda_grid < r_prime])
+    }
+    r <- max(lambda_grid[lambda_grid < r])
+  }
+  return (r_av)
+}
+
+
+
+
+l_max <- function (x) max(abs(x[-1, ]))
+max_lambda <- l_max(t(x) %*% y$target)/dim(x)[1] # we choose the maximum lambda to be the smallest lambda 
                                              #  for which all predictors are 0 (because the condition is trivially fulfilled for all higher lambdas), 
                                              #  see "A Practical Scheme and Fast Algorithm to Tune the Lasso With Optimality Guarantees" page 3
 
@@ -69,52 +123,25 @@ factor <- 3/4 # 3/(4n) is a good approximation of the factor for the lasso error
               #  according to fundamentals of high dimensional statistics example 4.4.1 (bottom of page 126)
               #  but since glmnet uses the mean squared error in their loss function we use 3/4 instead, as in 
               #  the original paper by chichignoud et. al. https://arxiv.org/abs/1410.0247
-r_av <- 0
-r_av_found <- FALSE
-r_test <- 0
 
-# we perform adaptive validation as in algorithm 4.4 from fumdamentals from high dimensional statistics
-while ((r != min(wide_grid)) && (!r_av_found)) {
-  beta_hat_r <- coef(beta_hats, s=r)
-  r_prime <- max(wide_grid)
-  message(sprintf("r: %s\n", r))
-  while ((r_prime > r) && (!r_av_found)) {
-    beta_hat_r_prime <- coef(beta_hats, s=r_prime)
-    
-    # debug
-    #message(sprintf("r: %s\n", r))
-    #message(sprintf("r_prime: %s\n", r_prime))
-    #message(sprintf("l(coef(beta_hat_r_prime) - coef(beta_hat_r)): %s\n", l(beta_hat_r_prime - beta_hat_r)))
-    #message(sprintf("factor * r_prime + factor * r: %s\n", factor * r_prime + factor * r))
-    
-    if (l(beta_hat_r_prime - beta_hat_r) > factor * r_prime + factor * r) {
-      r_av_found <- TRUE #exit from both loops 
-      
-      # before we exit we choose the last r for which the condition was still fulfilled
-      r <- min(wide_grid[wide_grid > r]) 
-      r_av <- r 
-    }
-    r_prime <- max(wide_grid[wide_grid < r_prime])
-  }
-  r <- max(wide_grid[wide_grid < r])
-}
-r_av
+
+# we use the maximum norm is used for lasso adaptive validation
+#  this is because we are trying to do support recovery and 
+#  because we use the maximum norm we can capture all "reasonably large predictors"
+#  see fundamentals of high dimensional statistics example 4.4.1 & chapter 7.4
+r.av.lmax <- adaptive_validation(l_max, wide_grid, beta_hats, factor)
+
+# we also try adaptive validation with the l1 norm as loss to see how this differs
+l1 <-function (x) sum(abs(x[-1, ]))
+r.av.l1 <- adaptive_validation(l1, wide_grid, beta_hats, factor)
+
 
 # now that we have our tuning parameter r_av we can fit on all our training data
-lasso.av.fit <- glmnet(x[train, ], y$target[train], alpha=1, lambda=r_av)
+lasso.av.lmax.fit <- glmnet(x[train, ], y$target[train], alpha=1, lambda=r.av.lmax)
+lasso.av.l1.fit <- glmnet(x[train, ], y$target[train], alpha=1, lambda=r.av.l1)
 
 
-# let's try doing cross validation too to compare, this should give us better prediction than adaptive validation
-set.seed(1)
-wide_grid <- 10^seq(-5, 1, length=200)
-cv.out <- cv.glmnet(x[train, ], y$target[train], nfolds=5, alpha=1, lambda=wide_grid)
-plot(cv.out)
 
-lasso.cv.min_lambda <- cv.out$lambda.min
-lasso.cv.min_lambda
-
-# we can then fit on all our data
-lasso.cv.fit <- glmnet(x[train, ], y$target[train], alpha=1, lambda=lasso.cv.min_lambda )
 
 
 # --- prediction ---
@@ -132,38 +159,46 @@ mean((lsquares.pred - y$target[test])^2)
 ridge.pred <- predict(ridge.fit, s=ridge.min_lambda, newx=x[test, ])
 mean((ridge.pred - y$target[test])^2)
 
-# lasso adaptive validation
-lasso.av.pred <- predict(lasso.av.fit, s=r_av, newx=x[test, ])
-mean((lasso.av.pred - y$target[test])^2)
-
 # lasso cross validation 
 lasso.cv.pred <- predict(lasso.cv.fit, s=lasso.cv.min_lambda, newx=x[test, ])
 mean((lasso.cv.pred - y$target[test])^2)
 
+# lasso adaptive validation with max norm loss
+lasso.av.lmax.pred <- predict(lasso.av.lmax.fit, s=r.av.lmax, newx=x[test, ])
+mean((lasso.av.lmax.pred - y$target[test])^2)
+
+# lasso adaptive validation with l1 norm loss
+lasso.av.l1.pred <- predict(lasso.av.l1.fit, s=r.av.l1, newx=x[test, ])
+mean((lasso.av.l1.pred - y$target[test])^2)
+
 # --- support recovery ---
 # let's look at our support recovery, which predictors did the different methods find and are these well known predictors?
 
-lsquares.predictors <- which(abs(coef(lsquares.fit)) > 0.05)[-1]
-ridge.predictors <- which(abs(coef(ridge.fit)) > 0.001)[-1]
+lsquares.predictors <- which(abs(coef(lsquares.fit)) > 0.03)[-1]
+ridge.predictors <- which(abs(coef(ridge.fit)) > 0.0008)[-1]
 lasso.cv.predictors <- which(coef(lasso.cv.fit) != 0)[-1]
-lasso.av.predictors <- which(coef(lasso.av.fit) != 0)[-1]
+lasso.av.lmax.predictors <- which(coef(lasso.av.lmax.fit) != 0)[-1]
+lasso.av.l1.predictors <- which(coef(lasso.av.l1.fit) != 0)[-1]
 
 lsquares.predictors
 ridge.predictors
 lasso.cv.predictors
-lasso.av.predictors
+lasso.av.lmax.predictors
+lasso.av.l1.predictors
 
 # and the overlapping predictors
-Reduce(intersect, list(v1 = lasso.cv.predictors, v2=lasso.av.predictors))
-
+Reduce(intersect, list(v1 = lasso.cv.predictors, v2=lasso.av.lmax.predictors))
+Reduce(intersect, list(v1 = lasso.cv.predictors, v2=lasso.av.lmax.predictors, v3=ridge.predictors))
+Reduce(intersect, list(v1 = lasso.cv.predictors, v2=lasso.av.predictors, v3=lasso.av.lmax.predictors))
+Reduce(intersect, list(v1 = lasso.cv.predictors, v2=lasso.av.predictors, v3=lasso.av.lmax.predictors, v4=ridge.predictors))
 
 # let's try doing least squares using only the predictors recovered from lasso with tuning parameter chosen by adaptive validation 
 
-lsquares.lav.fit <- glmnet(x[train, lasso.av.predictors], y$target[train], alpha=0, lambda=0)
-lsquares.lav.pred <- predict(lsquares.lav.fit, s=0, newx=x[test, lasso.av.predictors])
+lsquares.lav.fit <- glmnet(x[train, lasso.av.lmax.predictors], y$target[train], alpha=0, lambda=0)
+lsquares.lav.pred <- predict(lsquares.lav.fit, s=0, newx=x[test, lasso.av.lmax.predictors])
 mean((lsquares.lav.pred - y$target[test])^2)
 
-recovered_predictors <- names(data[1, 3:54677])[lasso.av.predictors]
+recovered_predictors <- names(data[1, 3:54677])[lasso.av.lmax.predictors]
 recovered_predictors
 
 # to find out which genes these probe identifiers correspond to we have to crossreference the annotation table
